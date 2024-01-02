024869c4ec77ff9f1598fb2b36a1fe6af610b2b4
SURENDAR <surendargovindraj02@gmail.com>
2024-01-02 20:38:29 +0530

    error in score calculation spoted-not solved

--- Result.py
+++ Result.py
@@ -1,24 +1,23 @@
 from tkinter import*
-def exit(user_score,computer_score):#show final scores and exit mainloop
+def final_scores(u_score,c_score):#show final scores and exit mainloop
     top1=Tk()
     top1.configure(bg="sky blue")
     top1.geometry("450x250")
     top1.title("result")
-        
-    lb1=Label(top1,text="your score:"+str(user_score),fg="yellow",bg="sky blue",font="times 20 bold",width=50)
-    lb1.pack()
-        
-    lb1=Label(top1,text="computer score:"+str(computer_score),fg="yellow",bg="sky blue",font="times 20 bold",width=50)
-    lb1.pack()
-        
-    if user_score==computer_score:
-        x="Tie!!"#save the final result in variable 'x'
-    elif user_score>computer_score:
+    
+    if u_score==c_score:
+        x="Tie!"#save the final result in variable 'x'
+    elif u_score>c_score:
         x="You Win!"
     else:
-        x="computer wins!"
+        x="Computer wins!"
             
-    lb1=Label(top1,text="result:"+x,fg="blue",bg="sky blue",font="times 35 bold",width=100)
+    lb1=Label(top1,text="Result:"+x,fg="blue",bg="sky blue",font="times 35 bold",width=100)
     lb1.pack()
-    root.destroy()   
+        
+    lb1=Label(top1,text="Your score:"+str(u_score),fg="green",bg="sky blue",font="times 20 bold",width=50)
+    lb1.pack()
+        
+    lb1=Label(top1,text="Computer score:"+str(c_score),fg="green",bg="sky blue",font="times 20 bold",width=50)
+    lb1.pack()
     top1.mainloop()
--- playroom.py
+++ playroom.py
@@ -1,14 +1,13 @@
 from tkinter import *
 import random
-from Result import exit
-user_score=0
-computer_score=0
+from Result import *
 def main():
     root = Tk()
     root.title("Welcome")
     root.geometry("1366x768")
     root.configure(bg="sky blue")
-
+    user_score=0 
+    computer_score=0
     label1 = Label(root, text="Welcome", fg="yellow",bg="dark blue", width=57, font="times 30 bold", height=2)
     label1.place(x=683, y=50, anchor="center")
 
@@ -23,51 +22,61 @@ def main():
 
     btn3 = Button(root, text="SCISSOR", font="times 20 bold", command=lambda:result("scissor"), fg="#4267B2", bg="white", padx=20, pady=15)
     btn3.place(x=1000, y=400)
-
-    btn4 = Button(root, text="EXIT", font="times 20 bold", command=lambda:exit(user_score,computer_score), fg="red", bg="white", padx=20, pady=15)
-    btn4.place(x=600, y=600)
     #functions
-    def declare_result(result,u_input,c_input):#show a quick result for each match
+    def declare_result(result,u_input,c_input,user_score,computer_score):#show a quick result for each match
         top=Toplevel(root)
         top.configure(bg="sky blue")
         top.geometry("450x250")
         top.title("result")
+        u_score=0
+        c_score=0
+        u_score+=user_score
+        c_score+=computer_score
         
-        lb1=Label(top,text="your input:"+u_input,fg="blue",bg="sky blue",font="times 20 bold",width=50)
+        lb1=Label(top,text="Your input:"+u_input,fg="green",bg="sky blue",font="times 20 bold",width=50)
         lb1.pack()
         
-        lb1=Label(top,text="computer input:"+c_input,fg="blue",bg="sky blue",font="times 20 bold",width=50)
+        lb1=Label(top,text="Computer input:"+c_input,fg="green",bg="sky blue",font="times 20 bold",width=50)
         lb1.pack()
         
         lb1=Label(top,text=result,fg="blue",bg="sky blue",font="times 35 bold",width=100)
         lb1.pack()
         
         btn1 = Button(top, text="Try again", font="times 20 bold", command=lambda:top.destroy(), fg="green", bg="white", padx=20, pady=15)
-        btn1.pack()
+        btn1.pack(side=LEFT)
+        
+        btn4 = Button(top, text="Scores", font="times 20 bold", command=lambda:final_scores(u_score,c_score), fg="red", bg="white", padx=20, pady=15)
+        btn4.pack(side=RIGHT) 
 
     def result(u_input):#calculte a result based on inputs
         options=["rock","paper","scissor"]
         c_input=random.choice(options)
         if u_input==c_input:
-            declare_result("It is a Tie!",u_input,c_input)
+            declare_result("It is a Tie!",u_input,c_input,user_score,computer_score)
             
         elif u_input=="rock":
             if c_input=="paper":
-                declare_result("Computer Win",u_input,c_input)
+                computer_score+=1
+                declare_result("Computer Win",u_input,c_input,user_score,computer_score)
             elif c_input=="scissor":
-                declare_result("You Win",u_input,c_input)
+                user_score+=1
+                declare_result("You Win",u_input,c_input,user_score,computer_score)
                 
         elif u_input=="paper":
             if c_input=="rock":
-                declare_result("You Win",u_input,c_input)
+                user_score+=1
+                declare_result("You Win",u_input,c_input,user_score,computer_score)
             elif c_input=="scissor":
-                declare_result("Computer Win",u_input,c_input)
+                computer_score+=1
+                declare_result("Computer Win",u_input,c_input,user_score,computer_score)
                 
         elif u_input=="scissor":
             if c_input=="paper":
-                declare_result("You win",u_input,c_input)
+                user_score+=1
+                declare_result("You win",u_input,c_input,user_score,computer_score)
             elif c_input=="rock":
-                declare_result("Computer Win",u_input,c_input)    
+                computer_score+=1
+                declare_result("Computer Win",u_input,c_input,user_score,computer_score)   
 # running the main loop
     root.mainloop()    
 if __name__ == '__main__':
